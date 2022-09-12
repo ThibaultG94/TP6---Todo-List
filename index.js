b7f7bdf7bc536fe700c183974c071f6dfb824fac
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-12 22:30:43 +0200

    checked list + remove list javascript

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("form");
 const input = document.querySelector(".form-input");
 const list = document.querySelector(".list");
+const taskList = document.querySelectorAll("li");
+let data = [];
+let i = 0;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,5 +14,25 @@ form.addEventListener("submit", (e) => {
   let textNode = document.createTextNode(todo);
 
   node.appendChild(textNode);
+  node.id = i;
   list.appendChild(node);
+
+  const newLi = [node.textContent, false];
+  data.push(newLi);
+  console.log(data);
+  i++;
+});
+
+list.addEventListener("click", (e) => {
+  const span = document.createElement("span");
+  span.textContent = " (terminé)";
+  let n = e.target.id;
+
+  if (data[e.target.id][1] === true) {
+    e.target.remove();
+  } else if (e.target && e.target.nodeName == "LI") {
+    e.target.style.listStyleImage = "url('./icons8-checked-12.png')";
+    e.target.appendChild(span);
+    data[e.target.id][1] = true;
+  }
 });
