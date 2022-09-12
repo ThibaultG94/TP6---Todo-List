8fce103df854d5a6af73ff70aef6343ed8f49e9b
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-12 18:47:23 +0200

    elements s'ajoutent à la liste

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const form = document.querySelector("form");
 const input = document.querySelector(".form-input");
+const list = document.querySelector(".list");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let todo1 = input.value;
+  let todo = input.value;
   input.value = "";
-  console.log(todo1);
+
+  const node = document.createElement("li");
+  let textNode = document.createTextNode(todo);
+
+  node.appendChild(textNode);
+  list.appendChild(node);
 });
