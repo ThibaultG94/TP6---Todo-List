451d58b3b03ac465f286b156e2fbe04c6a505249
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-13 18:33:53 +0200

    clean javascript

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const form = document.querySelector("form");
 const input = document.querySelector(".form-input");
 const list = document.querySelector(".list");
-const taskList = document.querySelectorAll("li");
+
 let data = [];
 let i = 0;
-window.localStorage.getItem(data);
-let mesDonnees = window.localStorage;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   let todo = input.value;
   input.value = "";
-
   const node = document.createElement("li");
   let textNode = document.createTextNode(todo);
-
   node.appendChild(textNode);
   node.id = i;
   list.appendChild(node);
 
   const newLi = [node.textContent, false];
   data.push(newLi);
-  console.log(data);
   i++;
-  window.localStorage.maBoite = data;
-  mesDonnees = window.localStorage.maBoite;
 });
 
 list.addEventListener("click", (e) => {
@@ -33,10 +27,7 @@ list.addEventListener("click", (e) => {
   if (data[n][1] === true) {
     e.target.remove();
   } else if (e.target && e.target.nodeName == "LI") {
-    e.target.style.listStyleImage = "url('./icons8-checked-12.png')";
     e.target.classList.add("checked");
     data[n][1] = true;
-    window.localStorage.maBoite = data;
-    mesDonnees = window.localStorage.maBoite;
   }
 });
