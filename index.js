385a00b06bcce4554e20cbb9a68bbc549e8dbabb
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-13 21:03:47 +0200

    version beta

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const form = document.querySelector("form");
 const input = document.querySelector(".form-input");
 const list = document.querySelector(".list");
 
-let data = [];
-let i = 0;
+// let data = [];
+// let i = 0;
 
 function storeList() {
   window.localStorage.todoList = list.innerHTML;
@@ -18,25 +18,25 @@ function getTodos() {
   }
 }
 
-function storeData() {
-  let todoData = [];
-  window.localStorage.todoData = data;
-  window.localStorage.totoI = i;
-}
+// function storeData() {
+//   let todoData = [];
+//   window.localStorage.todoData = data;
+//   window.localStorage.totoI = i;
+// }
 
-function loadData() {
-  let x = 0;
-  if (window.localStorage.todoData) {
-    data = [window.localStorage.todoData];
-    i = window.localStorage.totoI;
-  } else {
-    let data = [];
-    let i = 0;
-  }
-}
+// function loadData() {
+//   let x = 0;
+//   if (window.localStorage.todoData) {
+//     data = [window.localStorage.todoData];
+//     i = window.localStorage.totoI;
+//   } else {
+//     let data = [];
+//     let i = 0;
+//   }
+// }
 
 window.addEventListener("load", getTodos());
-window.addEventListener("load", loadData());
+// window.addEventListener("load", loadData());
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -46,36 +46,50 @@ form.addEventListener("submit", (e) => {
   const node = document.createElement("li");
   let textNode = document.createTextNode(todo);
   node.appendChild(textNode);
-  node.id = i;
+  // node.id = i;
   list.appendChild(node);
 
   const newLi = [node.textContent, false];
   data.push(newLi);
-  i++;
+  // i++;
   storeList();
-  storeData();
+  // storeData();
 });
 
 list.addEventListener("click", (e) => {
   let n = e.target.id;
 
-  if (e.target.classList.contains("test")) {
-    if (e.target.classList.contains("checked")) {
-      e.target.remove();
-      storeList();
-      storeData();
-    } else {
-      e.target.classList.add("checked");
-      storeList();
-      storeData();
-    }
-  } else if (data[e.target.id][1] === true) {
+  if (e.target.classList.contains("checked")) {
     e.target.remove();
     storeList();
-  } else if (e.target && e.target.nodeName == "LI") {
+    // storeData();
+  } else {
     e.target.classList.add("checked");
-    data[e.target.id][1] = true;
     storeList();
-    storeData();
+    // storeData();
   }
 });
+
+// list.addEventListener("click", (e) => {
+//   let n = e.target.id;
+
+//   if (e.target.classList.contains("test")) {
+//     if (e.target.classList.contains("checked")) {
+//       e.target.remove();
+//       storeList();
+//       storeData();
+//     } else {
+//       e.target.classList.add("checked");
+//       storeList();
+//       storeData();
+//     }
+//   } else if (data[e.target.id][1] === true) {
+//     e.target.remove();
+//     storeList();
+//   } else if (e.target && e.target.nodeName == "LI") {
+//     e.target.classList.add("checked");
+//     data[e.target.id][1] = true;
+//     storeList();
+//     storeData();
+//   }
+// });
